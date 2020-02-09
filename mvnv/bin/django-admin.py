a2daf8097b1d6581e3c032788fe902d3a0fba953
hwax0r <sergeevdavid38@gmail.com>
2020-02-09 18:55:31 +0300

    ok. here we go again

---
+++ mvnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hwax0r/Documents/projects/python/Django_Boyyyy/mvnv/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
